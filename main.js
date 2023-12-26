645340ac8eab6ca139c11ec352778a5a19e67f58
자도령 <141736145+Jadoryeong@users.noreply.github.com>
2023-12-26 14:01:59 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -11,23 +11,23 @@ const render = Render.create({ //렌더링
     wireframes: false, //기본값은 true로 되어 있지만 true로 되어 있을 경우 물리엔진을 시험해보는 화면으로 출력되게 되어 그래픽요소를 제외한 모습으로 보임 즉, 내가 설정한 색으로 안 보인다
     background: "#F7F4C8", //백그라운드 컬러(헥사 코드)
     width: 620, //게임 화면 너비
-    height: 740, //게임 화면 높이
+    height: 670, //게임 화면 높이
   }
 });
 
 const world = engine.world;
 
-const leftWall = Bodies.rectangle(15, 370, 30, 790, { //왼쪽벽, 괄호속에 x,y,너비, 높이 순으로 좌표가 들어감, 중앙을 기준으로 삼기에 오브젝트 길이의 중앙값으로 넣어줘야함
+const leftWall = Bodies.rectangle(15, 300, 30, 790, { //왼쪽벽, 괄호속에 x,y,너비, 높이 순으로 좌표가 들어감, 중앙을 기준으로 삼기에 오브젝트 길이의 중앙값으로 넣어줘야함
   isStatic: true, //기본적으로 물리엔진이 적용되어 있기때문에 true로 되어있지 않다면 벽이 계속 아래로 추락함
   render: { fillStyle: "#E6B143" }
 });
 
-const rightWall = Bodies.rectangle(605, 370, 30, 790, { //오른쪽벽, 괄호속에 x,y,너비, 높이 순으로 좌표가 들어감, 중앙을 기준으로 삼기에 오브젝트 길이의 중앙값으로 넣어줘야함
+const rightWall = Bodies.rectangle(605, 300, 30, 790, { //오른쪽벽, 괄호속에 x,y,너비, 높이 순으로 좌표가 들어감, 중앙을 기준으로 삼기에 오브젝트 길이의 중앙값으로 넣어줘야함
   isStatic: true, //기본적으로 물리엔진이 적용되어 있기때문에 true로 되어있지 않다면 벽이 계속 아래로 추락함
   render: { fillStyle: "#E6B143" }
 });
 
-const ground = Bodies.rectangle(310, 710, 620, 60, { //밑면, 괄호속에 x,y,너비, 높이 순으로 좌표가 들어감, 중앙을 기준으로 삼기에 오브젝트 길이의 중앙값으로 넣어줘야함
+const ground = Bodies.rectangle(310, 640, 620, 60, { //밑면, 괄호속에 x,y,너비, 높이 순으로 좌표가 들어감, 중앙을 기준으로 삼기에 오브젝트 길이의 중앙값으로 넣어줘야함
   isStatic: true, //기본적으로 물리엔진이 적용되어 있기때문에 true로 되어있지 않다면 벽이 계속 아래로 추락함
   render: { fillStyle: "#E6B143" }
 });
@@ -124,6 +124,12 @@ window.onkeyup = (event) => {
 
 Events.on(engine, "collisionStart", (event) => {
   event.pairs.forEach((collision) => {
+    if (
+      !disableAction &&
+      (collision.bodyA.name === "topLine" || collision.bodyB.name === "topLine")) {
+      alert("Game over");
+    }    
+    
     if (collision.bodyA.index === collision.bodyB.index) {  //만약 두개의 과일이 같다면
       const index = collision.bodyA.index;
 
@@ -150,11 +156,7 @@ Events.on(engine, "collisionStart", (event) => {
       World.add(world, newBody);
     }
 
-    if (
-      !disableAction &&
-      (collision.bodyA.name === "topLine" || collision.bodyB.name === "topLine")) {
-      alert("Game over");
-    }
+
   });
 });
 
